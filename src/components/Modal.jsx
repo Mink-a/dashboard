2f3e6247540e6aad8b1d04290469836ac4b663b8
Mink-a <rahulakhant@gmail.com>
2022-09-28 02:21:02 +0630

    modal close on click outside

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,9 +6,13 @@ function Modal({ setOpenModal }) {
     <div>
       <div>
         <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none backdrop-opacity-10 bg-black/30">
+          <div
+            className="absolute w-screen h-screen top-0 left-0 bg-black/20"
+            onClick={() => setOpenModal(false)}
+          ></div>
           <div className="relative w-auto my-6 mx-auto max-w-3xl">
             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-              <Form setShowForm={setOpenModal}/>
+              <Form setShowForm={setOpenModal} />
             </div>
           </div>
         </div>
