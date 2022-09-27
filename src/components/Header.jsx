377d782b8022b27f67833af97e1b784819a335f5
Mink-a <rahulakhant@gmail.com>
2022-09-27 20:26:46 +0630

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/Logo.png";
+import Admin from "../assets/Admin.png";
+import { FaBell } from "react-icons/fa";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="bg-[#54BAB9] flex items-center justify-end gap-10 px-4 py-2 text-white">
+      <img className="mr-auto" src={Logo} alt="logo" width={48} />
+      <FaBell className="text-2xl" />
+      <div className="flex gap-5">
+        <img src={Admin} alt="Admin" />
+        <div>
+          <h1 className="font-bold font-[Poppins]">Lisa</h1>
+          <p className="font-[Poppins]">Operator</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
